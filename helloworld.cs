fd2646e06c26b90c016a7ee4518123114242d2eb
ogmachii <39017778+ogmachii@users.noreply.github.com>
2018-07-20 16:35:57 -0400

    Added C# via Website - Drag and dropped
    Just a simple Hello World program in C#..

---
+++ helloworld.cs
@@ -0,0 +1,17 @@
+/**
+ * CC 
+ * A Hello World program in C# 
+ * With the help of Sololearn
+ **/
+using System;
+
+namespace HelloWorldProgram{
+ 
+ class HelloProgram{
+     
+     static void Main(string[] args){
+         console.WriteLine("Hello World!");
+     }
+     
+ }
+}

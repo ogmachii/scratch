296261a27336c264b3788d74f56317a7fa7732d5
ogmachii <39017778+ogmachii@users.noreply.github.com>
2018-07-22 01:56:38 -0400

    a QUICK update to the Helloworld.cs
    Just added a short while loop and a flow statement. Along with a const var.. No one will change the helloWorldStatement!

--- helloworld.cs
+++ helloworld.cs
@@ -1,6 +1,6 @@
 /**
  * CC 
- * A Hello World program in C# 
+ * A simple program in C# 
  * With the help of Sololearn
  **/
 using System;
@@ -10,8 +10,21 @@ namespace HelloWorldProgram{
  class HelloProgram{
      
      static void Main(string[] args){
-         console.WriteLine("Hello World!");
-     }
-     
+         const var hellowWorldStatement = "Hello World!"; 
+         console.WriteLine(hellowWorldStatement);
+
+        if (hellowWorldStatement == 10){
+            console.WriteLine("It's equal to the integer 10!!");
+        }else{
+            console.WriteLine("You have failed to solve it's equivalency.. :(");
+        }
+        boolean endLoop = false; 
+        while(!endLoop){
+            console.WriteLine("Going to end that loop..")
+            endLoop = true; 
+        }
+        return 0; 
+     }    
  }
+ 
 }

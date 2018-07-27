487367a05d0fb41f5dc150d6f4e695b2d8200ba1
ogmachii <39017778+ogmachii@users.noreply.github.com>
2018-07-26 20:56:43 -0400

    Fixed a major problem and added some more
    Just threw some flow in there and a random number

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,13 +4,28 @@
  * With the help of Sololearn
  **/
 #include <iostream>
+#include <cstdlib>
 using namespace std; 
+
 int main{
 
     const string helloWorldStatement = "Hello World!"; 
+    int i = 10;
+    int randomNumber = rand() % 15;
+    cout<<helloWorldStatement << endl;
+    while(i != 0){
+        cout<<"**" << helloWorldStatement << "**" << endl; 
+        i--;
+        for(int j = 0; j < 5; j++){
+            cout<<"Wowza You hit the for loop!: "<< j << endl;
+        }
+        do{
+            cout<<"Oh my hit the do while loop: "<< randomNumber << endl;
+            randomNumber--; 
+        }while(randomNumber > 0)
+    }
 
-    cout>>helloWorldStatement;
-    
+
     return 0;
 
 }
